33a2090405cbbcf629e3b20bb9a44bc03336354d
HIDDEN151 <0430kkskorean@gmail.com>
2026-05-11 02:46:55 +0900

    2063

--- SamsungCodeTest/sol.c
+++ SamsungCodeTest/sol.c
@@ -1,72 +1,91 @@
 #include <stdio.h>
 
-#define NUM             10U
-#define GET_T           getTestCase
-#define CLEAN_ARRAY     cleanGlobalArray
-#define GET_INPUT       getInput
-#define GET_RESULT      getResult
+const unsigned int VERSION[1U] = {2063U};
+int array[200U] = {0U};
+int N;
 
-int     vGArray[NUM];
+void (*sol)(void);
+void (*init)(void);
 
-int     getTestCase(void);
-void    printMessage(const char * msg);
-void    cleanGlobalArray(void);
-void    getInput(void);
-int     getResult(void);
+static void dummy(void);
+static void init_2063(void);
+static void sol_2063(void);
 
-int getResult(void)
+static void dummy(void)
 {
-    int ret     = 0U;
-    int first   = 0U;
-    int remain  = 0U;
-    for(int i = 0; i < NUM; ++i)
-    {
-        ret = ret + vGArray[i];        
-    }
-    first   = ret / 10U;
-    remain  = ret % 10U;
-    if(remain >= 5U)
-    {
-        first = first + 1U;        
-    }
-    return first;
+    ;
 }
 
-void getInput(void)
+static void init_2063(void)
 {
-    int tmp;
-    for(int i = 0; i < NUM; ++i)
+    int idx;
+    int num;
+    for(idx = 0; idx < 200U; ++idx)
     {
-        scanf("%d", &tmp);
-        vGArray[i] = tmp;
+        array[idx] = 0;
+    }
+    scanf("%d", &N);
+    for(idx=0; idx < N; ++idx)
+    {
+        scanf("%d", &num);
+        array[num] = 1;
     }
 }
 
-void cleanGlobalArray(void)
+static void sol_2063(void)
 {
-    for(int i = 0; i < NUM; i++)
+    int idx;
+    int num = 0;
+    int result = 0;
+    int cnt = 0;
+    for(idx = 0; idx < 200; ++idx)
     {
-        vGArray[i] = 0U;
+        if(1 == array[idx])
+        {
+            cnt = cnt + 1;            
+        }
     }
-}
+    for(idx = 0; idx < 200; ++idx)
+    {
+        if(1 == array[idx])
+        {
+            num = num + 1;            
+        }
+        if(cnt % 2 == 0)
+        {
+            if(num == ((cnt / 2) + 2))
+            {
+                result = idx;
+                break;
+            }
+        }
+        else
+        {
+            if(num == ((cnt / 2) + 1))
+            {
+                result = idx;
+                break;
+            }
 
-// (#1)
-int getTestCase(void)
-{
-    int ret;
-    scanf("%d", &ret);
-    return ret;
+        }
+    }
+    // printf("%d %d %d %d\n", N, num, result, ((N / 2) + 1));
+    printf("%d\n", result);
 }
 
 int main(int argc, char **argv)
 {
-    int T;
-    T = GET_T();
-    for(int i = 0; i < T; i++)
+    if(2063U == VERSION[0U])
     {
-        CLEAN_ARRAY();
-        GET_INPUT();
-        printf("#%d %d\n", (i+1), GET_RESULT());
+        init = init_2063;
+        sol = sol_2063;
     }
+    else
+    {
+        init = dummy;
+        sol = dummy;
+    }
+    init();
+    sol();
     return 0;
 }
