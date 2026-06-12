418e91726296b0e8d891fa33eda51325be21c3f0
HIDDEN151 <0430kkskorean@gmail.com>
2026-06-13 01:15:26 +0900

    ADD

--- Programmers/main.c
+++ Programmers/main.c
@@ -1,42 +1,20 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include "sol.h"
 
-#define NUM 181939
+int main (int argc, char **argv) {
+    if (problem == 181932) {
+        printf("181932\n");
+        printf("%s", solution_181932 ("abd"));
+    } else if (problem == 181850) {
+        printf("181932\n");
+        printf("%d\n", solution_181850(1.2));
+    } else {
+        /*noop*/
 
-#if (NUM == 181940)
-char* solution(const char* my_string, int k);
-#endif
+    }
 
-#if (NUM == 181938)
-int solution(int a, int b);
-#endif
-
-#if (NUM == 181939)
-int solution(int a, int b);
-#endif
-
-int main(int argc, char **argv)
-{
-    #if (NUM == 181941)
-    const char* tst1[3] = {"a","b","c"};
-    printf("%s", solution(tst1, 3));
-    #endif
-
-    #if (NUM == 181940)
-    const char * tst = "tst1";
-    printf("%s", solution(tst, 3));
-    #endif
-
-    #if (NUM == 181938)
-    printf("%d\n", solution(1, 3));
-    printf("%d\n", solution(3, 1));
-    #endif
-
-    #if (NUM == 181939)
-    printf("%d\n", solution(1, 3));
-    printf("%d\n", solution(3, 1));
-    #endif
 
     return 0;
 }
---
+++ Programmers/sol.h
@@ -0,0 +1,8 @@
+#ifndef SOL_H_
+#define SOL_H_
+
+extern const int problem;
+extern char* solution_181932 (const char* code);
+extern int solution_181850(double flo);
+
+#endif
