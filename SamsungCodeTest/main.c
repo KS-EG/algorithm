a4a48a60e51c8efd064e6e34e5a8472305763d78
HIDDEN151 <0430kkskorean@gmail.com>
2026-06-28 19:56:37 +0900

    ADD

---
+++ SamsungCodeTest/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include "sol.h"
+
+int main (int argc, char **argv) {
+    if (problem == 2056) {
+        printf ("2056\n");
+    } else {
+        /** noop */
+    }
+    return 0;
+}
---
+++ SamsungCodeTest/sol.h
@@ -0,0 +1,6 @@
+#ifndef SOL_H_
+#define SOL_H_
+
+extern const int problem;
+
+#endif
